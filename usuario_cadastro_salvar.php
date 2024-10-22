6d713572dbc5335f42feddc55dec3abc4e3ccacb
Satoguv <guscafer@gmail.com>
2024-10-22 09:30:08 -0300

    Não sei o que foi feito

--- usuario_cadastro_salvar.php
+++ usuario_cadastro_salvar.php
@@ -8,8 +8,35 @@
 <body>
     <!-- INCLUSÃO DO CABEÇALHO -->
     <?php include "header_admin.php" ?>
-    
+
     <!-- INCLUSÃO DO RODAPÉ -->
     <?php include "footer_admin.php" ?>
+
+    <?php 
+        //1º PASSO: Inclusão das funções de acesso a dados
+        include "conexao_bd.php";
+
+        //2º PASSO: Capturar o login e senha digitado pelo usuário
+        $login_usuario = $_POST["txtLoginUsuario"];
+        $senha_usuario = $_POST["txtSenhaUsuario"];
+
+        $hash = password_hash($senha_usuario,1);
+
+        //3º PASSO: Comando SQL
+        $sql = " INSERT INTO usuario(login_usuario,senha_usuario) ";
+        $sql .= "VALUES ('$login_usuario','$hash') ";
+
+        //4º PASSO: Executar no BDA
+        if(executarComando($sql))
+        {
+            echo "<h1>Usuário Adicionado</h1>";
+        }
+
+        else
+        {
+            echo "<h1>Usuário nao cadastrado</h1>";
+        }
+
+    ?>
 </body>
 </html>
